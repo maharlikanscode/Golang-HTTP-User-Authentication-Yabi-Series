76cbadd2e05b9250cb5ca83870e20e7351742544
Maharlikans Code <72076522+maharlikanscode@users.noreply.github.com>
2020-12-23 17:53:41 +0800

    Add files via upload

---
+++ config/db.go
@@ -0,0 +1,13 @@
+package config
+
+// DBName is the MySQL Database name for the app "mkc"
+const DBName = "maharlikanscode"
+
+// DBUserName is your database username
+const DBUserName = "root"
+
+// DBPassword is your database password
+const DBPassword = ""
+
+// DBHostName is your database server host name or IP address with port number, e.g 127.0.0.1:3306
+const DBHostName = "127.0.0.1:3306"
