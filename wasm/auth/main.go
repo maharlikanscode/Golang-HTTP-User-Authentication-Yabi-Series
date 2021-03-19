4ca7b1ba4967cd75b10797674ba162be1af66cc5
Maharlikans Code <maharlikanscode@gmail.com>
2021-03-19 08:08:24 +0800

    Add files via upload

--- wasm/auth/main.go
+++ wasm/auth/main.go
@@ -243,6 +243,98 @@ func register(this js.Value, args []js.Value) interface{} {
 	return nil
 }
 
+func passwordReset(this js.Value, args []js.Value) interface{} {
+	jsDoc := js.Global().Get("document")
+	if !jsDoc.Truthy() {
+		return js.Global().Call("eval", `Swal.fire("Oops!, Error", "Unable to get document objects", "error");`)
+	}
+	email := jsDoc.Call("getElementById", "email")
+	if !email.Truthy() {
+		return js.Global().Call("eval", `Swal.fire("Email is Required!", "Unable to get email object", "error");`)
+	}
+
+	// Get the CSRF Token value from the client side
+	csrfToken := jsDoc.Call("getElementById", "csrfToken")
+	if !csrfToken.Truthy() {
+		return js.Global().Call("eval", `Swal.fire("Oops!", "Unable to get the CSRF token", "error");`)
+	}
+	var pCSRFToken string = csrfToken.Get("value").String()
+
+	var pEmail string = email.Get("value").String()
+
+	// Compose the JSON post payload to teh API endpoint.
+	payLoad := map[string]interface{}{
+		"email": pEmail,
+	}
+
+	bytesRepresentation, err := json.Marshal(payLoad)
+	if err != nil {
+		return js.Global().Call("eval", `Swal.fire("Oops! Error", "Something went wrong during password reset process", "error");`)
+	}
+
+	// HTTP new request
+	siteHost := serverIP + "/api/v1/user/password_reset"
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf(siteHost), bytes.NewBuffer(bytesRepresentation))
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-CSRF-TOKEN", pCSRFToken)
+	if err != nil {
+		return js.Global().Call("eval", `Swal.fire("Oops! Error", "Something went wrong with your connection, please try again", "error");`)
+	}
+
+	var isSuccess bool = false
+
+	// Get the response from the http.NewRequest post method from a channel
+	c1 := make(chan map[string]interface{}, 1)
+	var result2 map[string]interface{}
+	go func() {
+		resp, _ := client.Do(req)
+		defer resp.Body.Close()
+		json.NewDecoder(resp.Body).Decode(&result2)
+		c1 <- result2 // send the response data to our channel name 'c'
+	}()
+
+	var result map[string]interface{}
+	go func() interface{} {
+		for {
+			timeout := make(chan bool, 1)
+			go func() {
+				time.Sleep(time.Second * 1)
+				timeout <- true
+			}()
+
+			select {
+			case result = <-c1:
+				i := result["IsSuccess"] // You must return with JSON format value of either 'true' or 'false' only.
+				mStatus := fmt.Sprint(i)
+				isSuccess, _ = strconv.ParseBool(mStatus)
+				alertTitle := fmt.Sprint(result["AlertTitle"])
+				alertMsg := fmt.Sprint(result["AlertMsg"])
+				alertType := fmt.Sprint(result["AlertType"])
+				redirectURL := fmt.Sprint(result["RedirectURL"])
+
+				msg := ""
+				if !isSuccess {
+					msg = `Swal.fire("` + alertTitle + `", "` + alertMsg + `", "` + alertType + `");` // error
+				} else {
+					msg = `Swal.fire("` + alertTitle + `", "` + alertMsg + `", "` + alertType + `");` // success
+				}
+				redirectTO := ""
+				if len(strings.TrimSpace(redirectURL)) > 0 {
+					redirectTO = `window.location.replace("` + redirectURL + `");`
+				}
+
+				return APIResponse(isSuccess, msg, redirectTO)
+				break
+			case <-timeout:
+
+			}
+		}
+	}()
+	return nil
+}
+
 // APIResponse is a generic swal response back to the JS client side
 func APIResponse(isSuccess bool, msg, redirectURL string) interface{} {
 	if !isSuccess {
@@ -257,6 +349,7 @@ func exposeGoFuncJS() {
 	// Exposing the following Go Functions to Javascript client side
 	js.Global().Set("login", js.FuncOf(login))
 	js.Global().Set("register", js.FuncOf(register))
+	js.Global().Set("passwordReset", js.FuncOf(passwordReset))
 }
 
 func main() {
